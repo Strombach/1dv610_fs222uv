2a704e12f99996c8cc5482126f9a83805f8e3b38
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-09-09 15:08:24 +0200

    final commit, still WIP

--- LAMP/www/Game.php
+++ LAMP/www/Game.php
@@ -4,8 +4,12 @@ class Game
 {
   private $sticksLeft = 21;
 
-  public $usedSticks;
-  
+  public $humanPicks = [];
+  public $humanString = "";
+
+  public $comPicks = [];
+  public $comString = "";
+
   public function __construct()
   {
     $this->startSession();
@@ -17,24 +21,50 @@ class Game
 
     $_SESSION["sticksLeft"] = $this->sticksLeft;
 
-    if (!isset($_SESSION["removedSticks"])) {
-      $_SESSION["removedSticks"] = [];
-    }
-    
     if (isset($_POST['selectSticks'])) {
-      array_push($_SESSION["removedSticks"], $_POST['selectSticks']);
-      $this->usedSticks = $this->getRemovedSticks();
+      $this->playRound();
     }
 
     if (isset($_POST['reset'])) {
-        $this->resetSession();
+      $this->resetSession();
     }
   }
-
 
   public function resetSession()
   {
     session_destroy();
+  }
+
+  public function playRound()
+  {
+    // HUMAN PART 
+    array_push($this->humanPicks, $_POST['selectSticks']);
+    $this->printSticks(false);
+
+    // COMPUTER PART 
+    if ($_SESSION["sticksLeft"] == 5) {
+      array_push($this->comPicks, 4);
+      $this->printSticks(true);
+    } else { 
+      array_push($this->comPicks, 1);
+      $this->printSticks(true);
+    }
+  }
+
+  // Gets every value in the session array.
+  public function printSticks($isAI)
+  {
+    if (!$isAI) {
+      foreach ($this->humanPicks as $pick) {
+        $this->humanString .= $pick . " ";
+        $_SESSION["sticksLeft"] -= $pick;
+      }
+    } else {
+      foreach ($this->comPicks as $pick) {
+        $this->comString .= $pick . " ";
+        $_SESSION["sticksLeft"] -= $pick;
+      }
+    }
   }
 
   public function show(): string
@@ -51,19 +81,10 @@ class Game
       <input type='submit'>
     </form>
     <br>
-    <p id='usedSticks'>$this->usedSticks</p>
+    <p id='humanPicks'>User drew: $this->humanString</p>
+    <p id='ComPicks'>Computer drew: $this->comString</p>
     <form method='POST'>
       <input name='reset' type='submit' value='Reset'>
     </form>";
-  }
-
-  // Gets every value in the session array.
-  public function getRemovedSticks(): string
-  {
-    $returnString = "";
-    foreach ($_SESSION["removedSticks"] as $removedStick) {
-      $returnString .= $removedStick . "  ";
-    }
-    return $returnString;
   }
 }
