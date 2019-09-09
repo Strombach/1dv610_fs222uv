825efb9cf8f3e54adfcefb29cef60c05fa00351d
Fredrik Strömbäck <strombach@pop-os.localdomain>
2019-09-09 10:55:34 +0200

    Adds README.md

--- LAMP/www/index.php
+++ LAMP/www/index.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["removedSticks"])) {
 
 // The form for selecting how many sticks to remove
 echo "
-$sticksLeft
+<p>Sticks left= $sticksLeft</p>
 <form action='' method='POST'>
   <select name='selectSticks'>
     <option value='1'>1</option>
@@ -22,10 +22,18 @@ $sticksLeft
   <input type='submit'>
 </form>";
 
+echo "
+<form action='' method='POST'>
+  <button name=type='button'>Reset</button>
+</form>";
+
 if(isset($_POST['selectSticks'])) {
   array_push($_SESSION["removedSticks"], $_POST['selectSticks']);
   displayRemovedSticks();
 }
+
+// ******** You should write a condition that checks of the form for reset
+// is pressed and Posted. In that case destroy session.
 
 // Echoes every value in the session array.
 function displayRemovedSticks() {
