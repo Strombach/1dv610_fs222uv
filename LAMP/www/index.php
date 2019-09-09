3b015062153f846865e744fad04dff59f43f685a
Fredrik Strömbäck <strombach@pop-os.localdomain>
2019-09-09 10:42:42 +0200

    Now displays every removedstick in the session

--- LAMP/www/index.php
+++ LAMP/www/index.php
@@ -1,7 +1,16 @@
 <?php
 
+session_start();
+
 $sticksLeft = 21;
 
+$_SESSION["sticksLeft"] = $sticksLeft;
+
+if (!isset($_SESSION["removedSticks"])) {
+  $_SESSION["removedSticks"] = [];
+}
+
+// The form for selecting how many sticks to remove
 echo "
 $sticksLeft
 <form action='' method='POST'>
@@ -13,5 +22,14 @@ $sticksLeft
   <input type='submit'>
 </form>";
 
-// echo $_POST["selectSticks"];
-echo $sticksLeft - $_POST["selectSticks"];
+if(isset($_POST['selectSticks'])) {
+  array_push($_SESSION["removedSticks"], $_POST['selectSticks']);
+  displayRemovedSticks();
+}
+
+// Echoes every value in the session array.
+function displayRemovedSticks() {
+  foreach($_SESSION["removedSticks"] as $removedStick) {
+    echo $removedStick;
+  }
+}
